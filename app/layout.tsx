4a938e95a8a382149d7285ab94b5f42fb2f75a13
nampgn <nampham18082003@gmail.com>
2025-07-12 09:47:19 +0700

    update next analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import "./globals.css";
 import { Providers } from "@/providers/Providers";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import { SeriesProvider } from "@/contexts/SeriesContext"
-import { Metadata } from "next"
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { SeriesProvider } from "@/contexts/SeriesContext";
+import { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +13,18 @@ export const metadata: Metadata = {
   metadataBase: new URL(`${process.env.NEXT_PUBLIC_SITE_URL}`),
   title: {
     default: "Hoạt hình trung quốc - Xem phim hoạt hình trung quốc mới nhất",
-    template: "%s | Hoạt hình trung quốc"
+    template: "%s | Hoạt hình trung quốc",
   },
-  description: "Xem phim hoạt hình trung quốc mới nhất, chất lượng cao, cập nhật nhanh chóng. Thưởng thức các bộ phim hoạt hình trung quốc hay nhất với phụ đề tiếng Việt.",
-  keywords: ["hoạt hình trung quốc", "hh3d", "anime trung quốc", "hoạt hình 3d trung quốc", "hh3dtq", "hhpanda"],
+  description:
+    "Xem phim hoạt hình trung quốc mới nhất, chất lượng cao, cập nhật nhanh chóng. Thưởng thức các bộ phim hoạt hình trung quốc hay nhất với phụ đề tiếng Việt.",
+  keywords: [
+    "hoạt hình trung quốc",
+    "hh3d",
+    "anime trung quốc",
+    "hoạt hình 3d trung quốc",
+    "hh3dtq",
+    "hhpanda",
+  ],
   authors: [{ name: "PH ANG" }],
   formatDetection: {
     email: false,
@@ -29,22 +37,26 @@ export const metadata: Metadata = {
     url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
     siteName: "Hoạt hình trung quốc",
     title: "Hoạt hình trung quốc - Xem phim hoạt hình trung quốc mới nhất",
-    description: "Xem phim hoạt hình trung quốc mới nhất, chất lượng cao, cập nhật nhanh chóng. Thưởng thức các bộ phim hoạt hình trung quốc hay nhất với phụ đề tiếng Việt.",
+    description:
+      "Xem phim hoạt hình trung quốc mới nhất, chất lượng cao, cập nhật nhanh chóng. Thưởng thức các bộ phim hoạt hình trung quốc hay nhất với phụ đề tiếng Việt.",
     images: [
       {
         url: "https://res.cloudinary.com/daz3lejjo/image/upload/f_webp/v1732691384/hoa-giang-ho-chi-bat-luong-nhan-phan-6-2-1_qkxcfi.jpg",
         width: 1200,
         height: 630,
-        alt: "Hoạt hình trung quốc"
-      }
-    ]
+        alt: "Hoạt hình trung quốc",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Hoạt hình trung quốc - Xem phim hoạt hình trung quốc mới nhất",
-    description: "Xem phim hoạt hình trung quốc mới nhất, chất lượng cao, cập nhật nhanh chóng. Thưởng thức các bộ phim hoạt hình trung quốc hay nhất với phụ đề tiếng Việt.",
-    images: ["https://res.cloudinary.com/daz3lejjo/image/upload/f_webp/v1732691384/hoa-giang-ho-chi-bat-luong-nhan-phan-6-2-1_qkxcfi.jpg"],
-    creator: "@ph_anh"
+    description:
+      "Xem phim hoạt hình trung quốc mới nhất, chất lượng cao, cập nhật nhanh chóng. Thưởng thức các bộ phim hoạt hình trung quốc hay nhất với phụ đề tiếng Việt.",
+    images: [
+      "https://res.cloudinary.com/daz3lejjo/image/upload/f_webp/v1732691384/hoa-giang-ho-chi-bat-luong-nhan-phan-6-2-1_qkxcfi.jpg",
+    ],
+    creator: "@ph_anh",
   },
   robots: {
     index: true,
@@ -52,7 +64,7 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-image-preview': 'large',
+      "max-image-preview": "large",
     },
   },
   verification: {
@@ -71,14 +83,14 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <body className={inter.className}>
-          <Providers>
-        <SeriesProvider>
+        <Providers>
+          <SeriesProvider>
             <Header />
             {children}
             <Footer />
-        </SeriesProvider>
-          </Providers>
-        <GoogleAnalytics gaId="G-0EEY3R7F0S" />
+          </SeriesProvider>
+        </Providers>
+        <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_G_ID}`} />
       </body>
     </html>
   );
