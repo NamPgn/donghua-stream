fad80de94ade57cc8b9f17f4d0d7d780e1f5b4c4
nampgn <nampham18082003@gmail.com>
2025-07-13 21:45:46 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,9 +51,6 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
     },
   },
   verification: {
