1923acabd4d00cb3314898e5dfbe7857bf155a23
PhamNamG <nampgph22148@gmail.com>
2025-08-03 21:11:17 +0700

    update

--- hooks/useSlider.ts
+++ hooks/useSlider.ts
@@ -2,6 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import { posterApi } from '@/services/api/poster.api';
 import type { Poster, PosterResponse } from '@/services/poster.server';
 
+
+
 export function useSlider() {
 	return useQuery<PosterResponse>({
 		queryKey: ['posters'],
